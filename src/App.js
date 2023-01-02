b109565c1f98f8d110b4c958a5ac60987258c5a6
Selim Sami <Selim.Genc@hhu.de>
2023-01-02 12:32:10 +0100

    added form section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 
 
@@ -23,6 +24,7 @@ function App (){
         <> 
             <Header />
             <div className='container'>
+                <FeedbackForm/>
                 <FeedbackStats feedback = {feedback} />
                 <FeedbackList feedback={feedback}
                 handleDelete={deleteFeedback}/> 
